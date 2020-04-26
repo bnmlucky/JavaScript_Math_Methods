03498bacb803be83671319e6a02fae9a1666e757
Natalia Titova <bnmlucky@gmail.com>
2020-04-26 18:26:46 -0400

    stated my variables and converted them into floats

--- js/math.js
+++ js/math.js
@@ -1,12 +1,17 @@
 // 1. Attach this file -- math.js -- to the index.html file using a <script> tag
 
 // 2. Add an alert to announce the program with a message like "Let's do some math!"
+alert("Let's do some math!");
 
 // 3. Create a variable and use the prompt() method to collect a number from a visitor
+var num1 = prompt("Please type a number");
 
 // 4. Convert that value from a string to a floating point number
+num1 = parseFloat(num1);
 
 // 5. Repeat steps 3 and 4 to create a second variable and collect a second number
+var num2 = prompt("Please type another number");
+num2 = parseFloat(num2);
 
 // 6. Create a new variable -- message -- which you'll use to build
 //    a complete message to print to the document
@@ -16,6 +21,7 @@
 //    the values input from the user. Use string concatenation to create this
 //    and make sure you actually perform the math on the values by 
 //    using the + symbol to add their values together
+var message = "<h1>Math with the numbers " + num1 + " and " + num2 + "</h1>";
 
 // 7. Add another string to the message variable. 
 //    The string should look something like this after concatenation:
